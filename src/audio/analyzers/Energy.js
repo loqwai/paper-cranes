82e04c5267c375912a97161575a918b6a92c3ef8
redaphid <iam@aaronherres/com>
2023-12-15 22:37:42 -1000

    prettier

--- src/audio/analyzers/Energy.js
+++ src/audio/analyzers/Energy.js
@@ -1,24 +1,24 @@
 // simple-processor.js
 class Energy extends AudioWorkletProcessor {
-  constructor() {
-      super();
-  }
-  process(inputs, outputs) {
-    let energy = 0;
-    // for all inputs
-    for (let input of inputs) {
-      // for all channels
-      for (let channel of input) {
-        // for all samples
-        for (let sample of channel) {
-          energy += Math.pow(Math.abs(sample), 2);
-        }
-      }
+    constructor() {
+        super()
     }
-    this.energy = energy;
-    this.port.postMessage(this.energy);
-    return true;
-  }
+    process(inputs, outputs) {
+        let energy = 0
+        // for all inputs
+        for (let input of inputs) {
+            // for all channels
+            for (let channel of input) {
+                // for all samples
+                for (let sample of channel) {
+                    energy += Math.pow(Math.abs(sample), 2)
+                }
+            }
+        }
+        this.energy = energy
+        this.port.postMessage(this.energy)
+        return true
+    }
 }
 
-registerProcessor('Audio-Energy', Energy);
+registerProcessor('Audio-Energy', Energy)
--- src/audio/analyzers/SpectralCentroid.js
+++ src/audio/analyzers/SpectralCentroid.js
@@ -1,23 +1,21 @@
-
 function mu(i, amplitudeSpect) {
-  let numerator = 0;
-  let denominator = 0;
+    let numerator = 0
+    let denominator = 0
 
-  for (let k = 0; k < amplitudeSpect.length; k++) {
-    numerator += Math.pow(k, i) * Math.abs(amplitudeSpect[k]);
-    denominator += amplitudeSpect[k];
-  }
+    for (let k = 0; k < amplitudeSpect.length; k++) {
+        numerator += Math.pow(k, i) * Math.abs(amplitudeSpect[k])
+        denominator += amplitudeSpect[k]
+    }
 
-  if (denominator === 0) return 0; // Prevent division by zero
-  return numerator / denominator;
+    if (denominator === 0) return 0 // Prevent division by zero
+    return numerator / denominator
 }
 
 function calculateSpectralCentroid(ampSpectrum) {
-  return mu(1, ampSpectrum);
+    return mu(1, ampSpectrum)
 }
 
-
-onmessage = function(event) {
-  const spectralCentroid = calculateSpectralCentroid(event.data);
-  postMessage(spectralCentroid);
-};
+onmessage = function (event) {
+    const spectralCentroid = calculateSpectralCentroid(event.data)
+    postMessage(spectralCentroid)
+}
