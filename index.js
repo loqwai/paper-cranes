22e6e927e26433e8ea556b81cd3331e92d11d02f
redaphid <iam@aaronherres/com>
2023-12-15 18:07:49 -1000

    node_modules etc for threejs

--- index.js
+++ index.js
@@ -1,36 +1,59 @@
-import {AudioProcessor} from './src/AudioProcessor.js';
-import * as glCanvas from './vendor/src/GlslCanvas.js';
-a
-const main = async () => {
-  const audioContext = new AudioContext();
-  await audioContext.resume();
-  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-  const sourceNode = audioContext.createMediaStreamSource(stream);
-  const audioProcessor = new AudioProcessor(audioContext, sourceNode);
-  document.querySelector('h1').remove();
-  // Remove event listeners if no longer needed
-  document.onclick = null;
-  document.ontouchstart = null;
-  document.onkeydown = null;
+import * as THREE from 'three';
+const container = document.getElementById('container');
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const scene = new THREE.Scene();
+const clock = new THREE.Clock();
+const uniforms = {
+    u_time: { value: 1.0 },
+    u_resolution: { value: new THREE.Vector2() },
+    u_mouse: { value: new THREE.Vector2() }
+};
+let renderer; // Declare renderer outside the init function
 
- const canvas = document.querySelector('#visualizer');
- const body = document.querySelector('body');
- body.classList.add('ready');
- const params = new URLSearchParams(window.location.search);
- const shader = params.get("shader");
- const initialImageUrl = params.get("image");
- if(!shader){
-   throw new Error("No shader specified");
- }
-//initialize GlSlCanvsas
-initializeCanvase(canvas);
- document.onclick = () => viz.startTime = performance.now();
- document.onkeydown = () => viz.startTime = performance.now();
+init();
+animate();
+
+function init() {
+    camera.position.z = 1;
+    const geometry = new THREE.PlaneGeometry(2, 2);
+
+    const material = new THREE.ShaderMaterial({
+        uniforms,
+        vertexShader: document.getElementById('vertexShader').textContent,
+        fragmentShader: document.getElementById('fragmentShader').textContent
+    });
+
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    renderer = new THREE.WebGLRenderer(); // Initialize renderer here
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    container.appendChild(renderer.domElement);
+
+    onWindowResize();
+    window.addEventListener('resize', onWindowResize);
+
+    document.onmousemove = (e) => {
+        uniforms.u_mouse.value.x = (e.clientX / window.innerWidth) * 2 - 1;
+        uniforms.u_mouse.value.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    };
 }
-document.onclick = main;
-document.onkeydown = main;
-document.ontouchstart = main;
 
-initialiaeCanvase = (canvas) =>{
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    uniforms.u_resolution.value.x = renderer.domElement.width;
+    uniforms.u_resolution.value.y = renderer.domElement.height;
+}
 
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+}
+
+function render() {
+    uniforms.u_time.value += clock.getDelta();
+    renderer.render(scene, camera);
 }
