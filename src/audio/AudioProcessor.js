f7d53dbe3a3e2654d99681b5c6800e5036e646a0
redaphid <iam@aaronherres/com>
2023-12-19 16:28:01 -1000

    flattened stats. Workers for SpectralCentroid + SpectralFlux

--- src/audio/AudioProcessor.js
+++ src/audio/AudioProcessor.js
@@ -20,12 +20,11 @@ export class AudioProcessor {
                 sourceNode.connect(audioProcessor)
                 audioProcessor.port.onmessage = (event) => (rawFeatures[processor] = event.data)
             }
-            for (const workerName of ['SpectralCentroid']) {
+            for (const workerName of ['SpectralCentroid', 'SpectralFlux']) {
                 const worker = new Worker(`/src/audio/analyzers/${workerName}.js?timestamp=${timestamp}`)
                 worker.onmessage = (event) => {
                     if (event.data.type === 'computedValue') {
-                        console.log(event.data)
-                        rawFeatures[workerName] = event.data.value
+                        rawFeatures[workerName] = event.data
                     }
                 }
                 worker.postMessage({ config: { historySize: 500 } })
@@ -43,16 +42,22 @@ export class AudioProcessor {
                 workers[worker].postMessage({ type: 'fftData', data: { fft: windowedFftData } })
             }
 
-            updateLegacyFeatures()
+            updateFeatures()
             requestAnimationFrame(pullFFTData)
         }
 
-        const updateLegacyFeatures = () => {
-            console.log(rawFeatures)
-            this.features['spectralSpread'] = rawFeatures['SpectralSpread'] || 0
-            this.features['spectralCentroid'] = (rawFeatures['SpectralCentroid'] || 0) / 4
-            this.features['spectralFlux'] = rawFeatures['SpectralFlux'] || 0
-            this.features['energy'] = rawFeatures['Energy'] || 0
+        const updateFeatures = () => {
+            // for each feature in raw features
+            for (const feature in rawFeatures) {
+                // the key in features is the same as the key in rawFeatures, except the first letter is lowercased
+                const featureKey = feature.charAt(0).toLowerCase() + feature.slice(1)
+                this.features[featureKey] = rawFeatures[feature].value
+                for (const propertyKey in rawFeatures[feature].stats) {
+                    // the key in features is the same as the key in rawFeatures, except the first letter is lowercased
+                    this.features[`${featureKey}${propertyKey.charAt(0).toUpperCase() + propertyKey.slice(1)}`] = rawFeatures[feature].stats[propertyKey]
+                }
+            }
+            console.log(this.features)
         }
 
         this.start = start
--- src/audio/analyzers/SpectralFlux.js
+++ src/audio/analyzers/SpectralFlux.js
@@ -1,5 +1,54 @@
-let previousSignal = null
+let historySize = 500, // Define history size
+    queue = [],
+    sum = 0,
+    sumOfSquares = 0,
+    min = Infinity,
+    max = -Infinity
 
+function set(value) {
+    if (typeof value !== 'number') throw new Error('Input must be a number')
+    min = Math.min(min, value)
+    max = Math.max(max, value)
+    queue.push(value)
+    sum += value
+    sumOfSquares += value * value
+
+    if (queue.length > historySize) {
+        let removed = queue.shift()
+        sum -= removed
+        sumOfSquares -= removed * removed
+        if (removed === min || removed === max) {
+            min = Math.min(...queue)
+            max = Math.max(...queue)
+        }
+    }
+
+    let mean = sum / queue.length,
+        variance = sumOfSquares / queue.length - mean * mean
+    return {
+        normalized: (value - min) / (max - min || 1),
+        mean: mean,
+        standardDeviation: Math.sqrt(variance),
+        zScore: (value - mean) / Math.sqrt(variance || 1),
+        min: min === Infinity ? -1 : min,
+        max: max === -Infinity ? -1 : max,
+    }
+}
+
+self.addEventListener('message', ({ data: e }) => {
+    if (e.type === 'fftData') {
+        let fftData = e.data.fft // Extract FFT data from message
+        let computed = calculateSpectralFlux(fftData) // Process FFT data
+        self.postMessage({ type: 'computedValue', value: computed, stats: set(computed) })
+    }
+    if (e.type === 'config') {
+        historySize = e.config.historySize
+    }
+})
+
+// calculations specific to the analyzer go here
+
+let previousSignal = null
 function calculateSpectralFlux(currentSignal) {
     if (!previousSignal) {
         previousSignal = currentSignal
@@ -16,9 +65,4 @@ function calculateSpectralFlux(currentSignal) {
     previousSignal = currentSignal
 
     return sf
-}
-
-onmessage = function (event) {
-    const spectralFlux = calculateSpectralFlux(event.data)
-    postMessage(spectralFlux)
 }
