911030e6ee2dd4792e0703542f2ec7cd6863fadc
redaphid <iam@aaronherres/com>
2023-12-22 00:20:17 -1000

    eyy it renders at least

--- src/Visualizer.js
+++ src/Visualizer.js
@@ -21,18 +21,13 @@ const getTexture = async (gl, url) => {
     })
 }
 export const makeVisualizer = async ({ canvas, shader, initialImageUrl }) => {
-    const gl = canvas.getContext('webgl2')
-    const ext = gl.getExtension('GMAN_debug_helper')
-    // if (ext) {
-    //     ext.setConfiguration({
-    //         warnUndefinedUniforms: false,
-    //     })
-    // }
+    const gl = canvas.getContext('webgl2', { antialias: false })
     const res = await fetch(`/shaders/${shader}.frag`)
+
     const fragmentShader = await res.text()
     const programInfo = twgl.createProgramInfo(gl, [vertexShader, fragmentShader])
-    let prevFrame = await getTexture(gl, initialImageUrl)
-
+    const frameBuffers = [twgl.createFramebufferInfo(gl), twgl.createFramebufferInfo(gl)]
+    // frameBufferInfo.attachments[0].texture = prevFrame
     const arrays = {
         position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
     }
@@ -40,28 +35,47 @@ export const makeVisualizer = async ({ canvas, shader, initialImageUrl }) => {
 
     gl.useProgram(programInfo.program)
 
-    let frame = 0
+    let frameNumber = 0
     const render = ({ time, audioFeatures }) => {
-        twgl.resizeCanvasToDisplaySize(gl.canvas)
-        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+        if (twgl.resizeCanvasToDisplaySize(gl.canvas)) {
+            for (const frameBuffer of frameBuffers) {
+                twgl.resizeFramebufferInfo(gl, frameBuffer)
+            }
+        }
+        const frame = frameBuffers[frameNumber % 2]
+
+        gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, frame.framebuffer)
+        gl.viewport(0, 0, frame.width, frame.height)
         const uniforms = {
             time,
-            resolution: [gl.canvas.width, gl.canvas.height],
-            prevFrame,
-            frame,
+            resolution: [frame.width, frame.height],
+            frame: frameNumber,
             ...audioFeatures,
         }
         twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo)
         twgl.setUniforms(programInfo, uniforms)
         twgl.drawBufferInfo(gl, bufferInfo)
-        prevFrame = twgl.createTexture(gl, {
-            src: canvas,
-            crossOrigin: 'anonymous',
-            mag: gl.LINEAR,
-            min: gl.LINEAR,
 
-        })
-        frame++
+        gl.bindFramebuffer(gl.READ_FRAMEBUFFER, frame.framebuffer)
+
+        // Bind the default framebuffer (null) as the DRAW framebuffer
+        gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, null)
+
+        // Blit (copy) the framebuffer to the canvas
+        gl.blitFramebuffer(
+            0,
+            0,
+            frame.width,
+            frame.height, // Source rectangle
+            0,
+            0,
+            gl.canvas.width,
+            gl.canvas.height, // Destination rectangle
+            gl.COLOR_BUFFER_BIT, // Mask (color buffer only)
+            gl.LINEAR, // Filter (linear for smooth scaling)
+        )
+
+        frameNumber++
     }
 
     return render
