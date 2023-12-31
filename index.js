62e0077b1f4c6a298178d30a726c0aa851911696
redaphid <iam@aaronherres/com>
2023-12-31 07:44:37 -0700

    disable cache aversion. Add fetch interceptor

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 import { AudioProcessor } from './src/audio/AudioProcessor.js'
 import { makeVisualizer } from './src/Visualizer.js'
-
+// import './index.css'
 const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 const events = ['click', 'touchstart', 'keydown', 'touchmove', 'touchstop']
 
 let ranMain = false
 let startTime = 0
+const params = new URLSearchParams(window.location.search)
+
+const interceptFetchAndCacheEverything = () => {
+    console.log('setting up fetch interceptor')
+    const originalFetch = window.fetch
+    window.fetch = async (url, options) => {
+        console.log('look at me, I am the fetch now')
+        //if the query parameter 'debug' is set, don't cache
+
+        if (params.has('debug')) {
+            return originalFetch(url, options)
+        }
+        const key = `cranes-${JSON.stringify({ url, options })}`
+        const cached = localStorage.getItem(key)
+        if (cached) {
+            console.log(`cache hit for ${url}`)
+            return new Response(cached)
+        }
+        const response = await originalFetch(url, options)
+        const responseClone = response.clone()
+        const text = await responseClone.text()
+        localStorage.setItem(key, text)
+        return response
+    }
+}
+interceptFetchAndCacheEverything()
 const main = async () => {
     if (ranMain) return
     window.cranes = window.cranes || {}
