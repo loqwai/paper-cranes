d835e47060ea721b397f0de6b07fba77dc2f1561
redaphid <iam@aaronherres.com>
2024-01-05 19:56:56 -0800

    update cache in the background

--- service-worker.js
+++ service-worker.js
@@ -6,9 +6,10 @@ const urlsToCache = [
     // Add other URLs and assets you want to cache
 ]
 console.log({ urlsToCache })
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('v1').then((cache) => {
+        caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(urlsToCache).catch((error) => {
                 console.error('Caching failed:', error)
                 // Optionally, handle the failure case, like skipping caching
@@ -16,21 +17,20 @@ self.addEventListener('install', (event) => {
         }),
     )
 })
+
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            // Return the cached response if found
-            if (response) {
-                return response
-            }
-
-            // Otherwise, fetch from the network and cache the response
-            return fetch(event.request).then((response) => {
-                return caches.open(CACHE_NAME).then((cache) => {
-                    cache.put(event.request, response.clone())
-                    return response
+        caches.match(event.request).then((cachedResponse) => {
+            // Use fetch to get the latest version from the network
+            const fetchPromise = fetch(event.request).then((networkResponse) => {
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, networkResponse.clone())
                 })
+                return networkResponse
             })
+
+            // Return the cached response immediately, if available, while the fetch continues in the background
+            return cachedResponse || fetchPromise
         }),
     )
 })
