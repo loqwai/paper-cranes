ae3d31dde8d582579e4b2534ad3dc38317ad96e9
hypnodroid <iam@aaronherres.com>
2024-02-12 22:32:41 -0700

    moved shader defaulting into edit.js

--- edit.js
+++ edit.js
@@ -8,12 +8,12 @@ const SAVE_CODE_FILENAME = 'cranes-manual-code'
 const DEFAULT_SHADER = `uniform float knob_1;
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-// Normalized pixel coordinates (from 0 to 1)
-vec2 uv = fragCoord/iResolution.xy;
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = fragCoord/iResolution.xy;
 
-vec3 col = vec3(1.,knob_1/2.,energy);
-// Output to screen
-fragColor = vec4(col,1.0);
+    vec3 col = vec3(1.,knob_1/2.,energy);
+    // Output to screen
+    fragColor = vec4(col,1.0);
 }`
 const FeatureEditor = ({ name, min, max, value, onChange, onDelete }) => {
     const handleValueChange = (e) => {
