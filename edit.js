8c77eabc99694492bf84c60fe29fb48b6eddc8a0
hypnodroid <iam@aaronherres.com>
2024-02-12 20:00:27 -0700

    use fragment; move to grid

--- edit.js
+++ edit.js
@@ -1,4 +1,4 @@
-import { render } from 'preact'
+import { render, Fragment } from 'preact'
 import { useState, useCallback, useEffect } from 'preact/hooks'
 import { html } from 'htm/preact'
 
@@ -7,39 +7,35 @@ window.cranes.setState = () => {} // Will be properly initialized below
 const SAVE_FILE_NAME = 'cranes-manual-features'
 
 const FeatureAdder = () => {
+    const [features, setFeatures] = useState({})
+    useEffect(() => {
+        setFeatures(JSON.parse(localStorage.getItem(SAVE_FILE_NAME) || '{"test": {"min": -3, "max": 3, "value": 1}}'))
+    }, [])
     return html`
-        <div id="feature-editor">
-            hi
+        <Fragment>
             <div className="new-feature">
-                <input type="text" placeholder="Enter new feature name" value=${newFeatureName} onInput=${(e) => setNewFeatureName(e.target.value)} />
-                <button type="button" onClick=${addNewFeature}>Add Feature</button>
+                <input type="text" placeholder="Enter new feature name" value="test" />
+                <button type="button">Add Feature</button>
             </div>
             <form>
                 ${Object.entries(features).map(
                     ([name, value]) => html`
                         <div className="edit-feature" key=${name}>
                             <label>${name}:</label>
-                            <input type="number" value=${sliderRanges[name]?.min ?? -3} onInput=${(e) => updateSliderRange(name, e.target.value, sliderRanges[name]?.max ?? 3)} />
-                            <input
-                                type="range"
-                                min=${sliderRanges[name]?.min ?? -3}
-                                max=${sliderRanges[name]?.max ?? 3}
-                                value=${value ?? 1}
-                                step="0.01"
-                                onInput=${(e) => updateFeatureValue(name, e.target.value)}
-                            />
-                            <span> (${value})</span>
-                            <input type="number" value=${sliderRanges[name]?.max ?? 1} onInput=${(e) => updateSliderRange(name, sliderRanges[name]?.min ?? -1, e.target.value)} />
-                            <br />
+                            <input class="min-feature-value" type="number" value="${-3}}" />
+                            <input class="feature-value" type="range" min="${-3}" max=${1} value=${value ?? 1} step="0.01" />
+                            <span> (${value ?? 1})</span>
+                            <input class="max-feature-value" type="number" value=${1} />
+                            <button>x</button>
                         </div>
                     `,
                 )}
             </form>
             <div className="save-load">
-                <button type="button" onClick=${saveCode}>Save</button>
-                <button type="button" onClick=${clearCode}>Reset</button>
+                <button type="button">Save</button>
+                <button type="button">Reset</button>
             </div>
-        </div>
+        </Fragment>
     `
 }
 
