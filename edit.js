9eef42789a9363edb9b8af2e91e7b0ee501e1a33
hypnodroid <iam@aaronherres.com>
2024-02-12 20:52:01 -0700

    got very basic setfeaturename working

--- edit.js
+++ edit.js
@@ -8,34 +8,35 @@ const SAVE_FILE_NAME = 'cranes-manual-features'
 
 const FeatureAdder = () => {
     const [features, setFeatures] = useState({})
+    const [newFeatureName, setNewFeatureName] = useState('')
+    const addNewFeature = () => {
+        if (!newFeatureName) alert('Feature name cannot be empty')
+        // setFeatures({ ...features, [newFeatureName]: { min: -3, max: 3, value: 1 } })
+        setNewFeatureName('')
+        console.log('features', features)
+    }
+
+    const testAddFeature = (e) => {
+        setNewFeatureName(e.target.value)
+    }
+
     useEffect(() => {
-        setFeatures(JSON.parse(localStorage.getItem(SAVE_FILE_NAME) || '{"test": {"min": -3, "max": 3, "value": 1}, "test2": {"min": -3, "max": 3, "value": 1}}'))
+        setFeatures(JSON.parse(localStorage.getItem(SAVE_FILE_NAME) || '{"knob_1": {"min": -3, "max": 3, "value": 1}, "test2": {"min": -3, "max": 3, "value": 1}}'))
     }, [])
     return html`
-        <Fragment>
+        <${Fragment}>
             <div className="new-feature">
-                <input type="text" placeholder="Enter new feature name" value="test" />
-                <button type="button">Add Feature</button>
+                <input type="text" value=${newFeatureName} onChange=${(e) => setNewFeatureName(e.target.value)} placeholder="Enter new feature name" />
+                <button type="button" onClick=${addNewFeature}>Add Feature</button>
             </div>
-            <form id="existing-features-editor">
-                ${Object.entries(features).map(
-                    ([name, value]) => html`
-                        <div className="edit-feature" key=${name}>
-                            <label>${name}:</label>
-                            <input class="min-feature-value" type="number" value=${-3} />
-                            <input class="feature-value" type="range" min="${-3}" max=${1} value=${value ?? 1} step="0.01" />
-                            <span> (${value ?? 1})</span>
-                            <input class="max-feature-value" type="number" value=${1} />
-                            <button>x</button>
-                        </div>
-                    `,
-                )}
-            </form>
+            <div id="existing-features-editor">
+
+            </div>
             <div className="save-load">
                 <button type="button">Save</button>
                 <button type="button">Reset</button>
             </div>
-        </Fragment>
+        </${Fragment}>
     `
 }
 
