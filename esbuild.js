88898fb3f20f9b62d74953c642b3363a38da028e
redaphid <iam@aaronherres/com>
2024-02-25 19:24:13 -0700

    move moar things to utils and wip. update esbuild to use esmodule syntax in build fine

--- esbuild.js
+++ esbuild.js
@@ -1,31 +1,29 @@
-const esbuild = require('esbuild')
-const path = require('path')
-const ncp = require('ncp').ncp
-const fs = require('fs')
-// Define your entry points
+import { build } from 'esbuild'
+import { join } from 'path'
+import { readdirSync } from 'fs'
+import * as ncp from 'ncp'
+
 const entryPoints = ['index.js', 'edit.js', 'service-worker.js']
-// add all files in src/ as entry points
-fs.readdirSync('./src', { recursive: true }).forEach((file) => {
+readdirSync('./src', { recursive: true }).forEach((file) => {
     if (file.endsWith('.js')) {
         entryPoints.push(`src/${file}`)
     }
 })
-// esbuild configuration for tree-shaking
-esbuild
-    .build({
-        entryPoints: entryPoints,
-        format: 'esm',
-        bundle: true,
-        minify: true,
-        sourcemap: process.env.NODE_ENV ? false : true,
-        outdir: path.join(__dirname, 'dist'),
-        treeShaking: true, // The essence of your quest,
-        define: {
-            CACHE_NAME: `"cranes-cache-v1"`,
-        },
-    })
-    .then(() => {
-        return Promise.all([
+
+build({
+    entryPoints: entryPoints,
+    format: 'esm',
+    bundle: true,
+    minify: true,
+    sourcemap: !process.env.NODE_ENV,
+    outdir: join(process.cwd(), 'dist'),
+    treeShaking: true,
+    define: {
+        CACHE_NAME: '"cranes-cache-v1"',
+    },
+})
+    .then(() =>
+        Promise.all([
             ncp('index.html', 'dist/index.html'),
             ncp('index.css', 'dist/index.css'),
             ncp('edit.html', 'dist/edit.html'),
@@ -33,6 +31,6 @@ esbuild
             ncp('favicon.ico', 'dist/favicon.ico'),
             ncp('images', 'dist/images'),
             ncp('shaders', 'dist/shaders'),
-        ])
-    })
+        ]),
+    )
     .catch(console.error)
