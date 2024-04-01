c25559b18910f627573551cef51e49fd9af5bf53
hypnodroid <iam@aaronherres.com>
2024-03-31 22:48:09 -0700

    spectralKurtosis

--- src/audio/analyzers/SpectralKurtosis.js
+++ src/audio/analyzers/SpectralKurtosis.js
@@ -1,45 +1,14 @@
-import { makeCalculateStats } from '../../utils/calculateStats'
+import { makeCalculateStats, spectralKurtosis } from 'hypnosound'
 
 let calculateStats = makeCalculateStats()
 
 self.addEventListener('message', ({ data: e }) => {
     if (e.type === 'fftData') {
-        let fftData = e.data.fft // Extract FFT data from message
-        let computed = calculateSpectralKurtosis(fftData) // Compute spectral kurtosis
-        if (computed === null) return
-        self.postMessage({ type: 'computedValue', value: computed, stats: calculateStats(computed) })
+        const { fft } = e.data // Extract FFT data from message
+        const value = spectralKurtosis(fft) // Compute spectral kurtosis
+        self.postMessage({ type: 'computedValue', value, stats: calculateStats(value) })
     }
     if (e.type === 'config') {
         calculateStats = makeCalculateStats(e.config.historySize)
     }
 })
-
-function mu(i, amplitudeSpect) {
-    let numerator = 0
-    let denominator = 0
-
-    for (let k = 0; k < amplitudeSpect.length; k++) {
-        numerator += Math.pow(k, i) * Math.abs(amplitudeSpect[k])
-        denominator += amplitudeSpect[k]
-    }
-
-    if (denominator === 0) return null // Prevent division by zero
-    return numerator / denominator
-}
-function calculateSpectralKurtosis(fftData) {
-    const mean = mu(1, fftData)
-    const secondMoment = mu(2, fftData)
-    const variance = secondMoment - Math.pow(mean, 2)
-
-    let fourthMoment = 0
-    for (let i = 0; i < fftData.length; i++) {
-        fourthMoment += Math.pow(fftData[i] - mean, 4)
-    }
-    fourthMoment /= fftData.length
-
-    // Add a small epsilon to the denominator to prevent gigantic scores when variance is very small
-    const epsilon = 1e-7 // Adjust epsilon based on the scale of your data
-    const kurtosis = variance ? fourthMoment / Math.pow(variance + epsilon, 2) - 3 : 0
-    if (kurtosis > 3000) return 0
-    return kurtosis / 3000
-}
