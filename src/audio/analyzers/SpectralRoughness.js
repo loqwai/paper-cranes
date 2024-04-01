bd52a05e66bc83e559555b825bf5e8725416181d
hypnodroid <iam@aaronherres.com>
2024-03-31 22:55:05 -0700

    spectralRoughness

--- src/audio/analyzers/SpectralRoughness.js
+++ src/audio/analyzers/SpectralRoughness.js
@@ -1,27 +1,11 @@
-import { makeCalculateStats } from '../../utils/calculateStats'
+import { makeCalculateStats, spectralRoughness } from 'hypnosound'
 
 let calculateStats = makeCalculateStats()
-
-function calculateSpectralRoughness(fftData) {
-    let roughness = 0
-
-    for (let i = 1; i < fftData.length; i++) {
-        // Calculate the difference in amplitude between adjacent frequency bins
-        let diff = Math.abs(fftData[i] - fftData[i - 1])
-        roughness += diff
-    }
-
-    return roughness
-}
-
 self.addEventListener('message', ({ data: e }) => {
     if (e.type === 'fftData') {
-        let fftData = e.data.fft // Extract FFT data from message
-
-        // Process FFT data for roughness
-        let computedRoughness = calculateSpectralRoughness(fftData)
-        computedRoughness /= 100_000
-        self.postMessage({ type: 'computedValue', value: computedRoughness, stats: calculateStats(computedRoughness) })
+        const { fft } = e.data // Extract FFT data from message
+        const value = spectralRoughness(fft) // Compute spectral roughness
+        self.postMessage({ type: 'computedValue', value, stats: calculateStats(value) })
     }
     if (e.type === 'config') {
         calculateStats = makeCalculateStats(e.config.historySize)
