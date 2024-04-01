813965bdd6443b02fe859a373009ef4b28cdae77
hypnodroid <iam@aaronherres.com>
2024-03-31 22:34:35 -0700

    spectralEntropy

--- src/audio/analyzers/SpectralEntropy.js
+++ src/audio/analyzers/SpectralEntropy.js
@@ -4,8 +4,8 @@ let calculateStats = makeCalculateStats()
 
 self.addEventListener('message', ({ data: e }) => {
     if (e.type === 'fftData') {
-        let fftData = e.data.fft // Extract FFT data from message
-        const value = spectralEntropy(fftData) // Compute spectral kurtosis
+        let fft = e.data.fft // Extract FFT data from message
+        const value = spectralEntropy(fft) // Compute spectral kurtosis
         self.postMessage({ type: 'computedValue', value, stats: calculateStats(value) })
     }
     if (e.type === 'config') {
