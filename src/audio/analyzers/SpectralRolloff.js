3f2577f4ed3e425d016ad07a08749bcb58f2118a
hypnodroid <iam@aaronherres.com>
2024-03-31 22:52:35 -0700

    spectralRolloff

--- src/audio/analyzers/SpectralRolloff.js
+++ src/audio/analyzers/SpectralRolloff.js
@@ -1,12 +1,12 @@
-import { makeCalculateStats } from '../../utils/calculateStats'
+import { makeCalculateStats, spectralRolloff } from 'hypnosound'
 
 let calculateStats = makeCalculateStats()
 
 self.addEventListener('message', ({ data: e }) => {
     if (e.type === 'fftData') {
-        let fftData = e.data.fft // Extract FFT data from message
-        let rolloff = calculateSpectralRolloff(fftData) // Compute Spectral Rolloff
-        self.postMessage({ type: 'computedValue', value: rolloff, stats: calculateStats(rolloff) })
+        const { fft } = e.data // Extract FFT data from message
+        const value = spectralRolloff(fft) // Compute spectral rolloff
+        self.postMessage({ type: 'computedValue', value, stats: calculateStats(value) })
     }
     if (e.type === 'config') {
         calculateStats = makeCalculateStats(e.config.historySize)
