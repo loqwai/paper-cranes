f9467b6d217ecf89066ec2f7c08c5cd00d34cdbd
redaphid <iam@aaronherres/com>
2024-06-14 13:30:09 -0700

    add shader list generator

--- esbuild.js
+++ esbuild.js
@@ -1,36 +1,80 @@
 import { build } from 'esbuild'
-import { join } from 'path'
-import { readdirSync } from 'fs'
+import { join, relative } from 'path'
+import { readdir, stat, writeFile } from 'fs/promises'
 import ncp from 'ncp'
-const entryPoints = ['index.js', 'edit.js', 'service-worker.js']
-readdirSync('./src', { recursive: true }).forEach((file) => {
-    if (file.endsWith('.js')) {
-        entryPoints.push(`src/${file}`)
-    }
-})
+import { promisify } from 'util'
 
-build({
-    entryPoints: entryPoints,
-    format: 'esm',
-    bundle: true,
-    minify: true,
-    sourcemap: !process.env.NODE_ENV,
-    outdir: join(process.cwd(), 'dist'),
-    treeShaking: true,
-    define: {
-        CACHE_NAME: '"cranes-cache-v1"',
-    },
-})
-    .then(() =>
-        Promise.all([
-            ncp('index.html', 'dist/index.html'),
-            ncp('index.css', 'dist/index.css'),
-            ncp('edit.html', 'dist/edit.html'),
-            ncp('edit.css', 'dist/edit.css'),
-            ncp('favicon.ico', 'dist/favicon.ico'),
-            ncp('images', 'dist/images'),
-            ncp('shaders', 'dist/shaders'),
-            ncp('codicon.ttf', 'dist/codicon.ttf'),
-        ]),
+const ncpAsync = promisify(ncp)
+
+async function getShaderFiles(dir) {
+    let fileList = []
+    const files = await readdir(dir)
+    await Promise.all(
+        files.map(async (file) => {
+            const filePath = join(dir, file)
+            const stats = await stat(filePath)
+            if (stats.isDirectory()) {
+                if (!['private', 'wip', 'knobs', 'utils', 'practice'].includes(file)) {
+                    const subDirFiles = await getShaderFiles(filePath)
+                    fileList = fileList.concat(subDirFiles)
+                }
+            } else if (file.endsWith('.frag')) {
+                fileList.push(filePath)
+            }
+        }),
     )
-    .catch(console.error)
+    return fileList
+}
+
+async function generateHTML(shaderFiles) {
+    let htmlContent = '<!DOCTYPE html>\n<html>\n<head>\n<title>Shaders</title>\n</head>\n<body>\n<ul>\n'
+    shaderFiles.forEach((file) => {
+        const relativePath = relative('shaders', file)
+        const queryParam = relativePath.replace(/\\/g, '/').replace('.frag', '')
+        htmlContent += `<li><a href="/?shader=${queryParam}">${queryParam}</a></li>\n`
+    })
+    htmlContent += '</ul>\n</body>\n</html>'
+
+    await writeFile(join('dist', 'shaders.html'), htmlContent)
+}
+
+async function main() {
+    const entryPoints = ['index.js', 'edit.js', 'service-worker.js']
+    const srcFiles = await readdir('./src', { withFileTypes: true })
+    srcFiles.forEach((file) => {
+        if (file.isFile() && file.name.endsWith('.js')) {
+            entryPoints.push(`src/${file.name}`)
+        }
+    })
+
+    const shaderDir = 'shaders'
+    const shaderFiles = await getShaderFiles(shaderDir)
+
+    await generateHTML(shaderFiles)
+
+    await build({
+        entryPoints: entryPoints,
+        format: 'esm',
+        bundle: true,
+        minify: true,
+        sourcemap: !process.env.NODE_ENV,
+        outdir: join(process.cwd(), 'dist'),
+        treeShaking: true,
+        define: {
+            CACHE_NAME: '"cranes-cache-v1"',
+        },
+    })
+
+    await Promise.all([
+        ncpAsync('index.html', 'dist/index.html'),
+        ncpAsync('index.css', 'dist/index.css'),
+        ncpAsync('edit.html', 'dist/edit.html'),
+        ncpAsync('edit.css', 'dist/edit.css'),
+        ncpAsync('favicon.ico', 'dist/favicon.ico'),
+        ncpAsync('images', 'dist/images'),
+        ncpAsync('shaders', 'dist/shaders'),
+        ncpAsync('codicon.ttf', 'dist/codicon.ttf'),
+    ])
+}
+
+main().catch(console.error)
