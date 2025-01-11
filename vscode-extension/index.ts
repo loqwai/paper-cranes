91cadeaf28db16a99c5de86169ed248321fe55e7
redaphid <iam@aaronherres.com>
2025-01-10 21:17:48 -0700

    debugging. adding grammar

--- vscode-extension/index.ts
+++ vscode-extension/index.ts
@@ -1,69 +1,234 @@
 import * as vscode from "vscode";
 
+console.log("🏮 Paper Cranes Extension Exists 🏮");
 export function activate(context: vscode.ExtensionContext) {
-  console.log("Paper Cranes extension activated");
+  console.log("🏮 Paper Cranes Extension Activated 🏮");
+  // Create disposable array for cleanup
+  const disposables: vscode.Disposable[] = [];
 
-  // Register completions provider for Paper Cranes specific features
-  const provider = vscode.languages.registerCompletionItemProvider("paper-cranes-fragment-shader", {
+  // Register completions provider
+  const completionProvider = vscode.languages.registerCompletionItemProvider("paper-cranes-fragment-shader", {
     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+      console.log("provideCompletionItems");
       const keywords = [
         // Audio analysis uniforms
         "spectralCentroid",
         "spectralCentroidNormalized",
+        "spectralCentroidMean",
+        "spectralCentroidMedian",
+        "spectralCentroidStandardDeviation",
+        "spectralCentroidZScore",
+        "spectralCentroidMin",
+        "spectralCentroidMax",
+
         "spectralFlux",
         "spectralFluxNormalized",
-        "spectralKurtosis",
-        "spectralKurtosisNormalized",
+        "spectralFluxMean",
+        "spectralFluxMedian",
+        "spectralFluxStandardDeviation",
+        "spectralFluxZScore",
+        "spectralFluxMin",
+        "spectralFluxMax",
+
+        "spectralSpread",
+        "spectralSpreadNormalized",
+        "spectralSpreadMean",
+        "spectralSpreadMedian",
+        "spectralSpreadStandardDeviation",
+        "spectralSpreadZScore",
+        "spectralSpreadMin",
+        "spectralSpreadMax",
+
+        "spectralRolloff",
+        "spectralRolloffNormalized",
+        "spectralRolloffMean",
+        "spectralRolloffMedian",
+        "spectralRolloffStandardDeviation",
+        "spectralRolloffZScore",
+        "spectralRolloffMin",
+        "spectralRolloffMax",
+
         "spectralRoughness",
         "spectralRoughnessNormalized",
-        "spectralSpread",
-        "spectralRolloff",
-        "spectralSkewness",
-        "spectralSkewnessNormalized",
+        "spectralRoughnessMean",
+        "spectralRoughnessMedian",
+        "spectralRoughnessStandardDeviation",
+        "spectralRoughnessZScore",
+        "spectralRoughnessMin",
+        "spectralRoughnessMax",
+
+        "spectralKurtosis",
+        "spectralKurtosisNormalized",
+        "spectralKurtosisMean",
+        "spectralKurtosisMedian",
+        "spectralKurtosisStandardDeviation",
+        "spectralKurtosisZScore",
+        "spectralKurtosisMin",
+        "spectralKurtosisMax",
+
         "energy",
+        "energyNormalized",
+        "energyMean",
+        "energyMedian",
+        "energyStandardDeviation",
+        "energyZScore",
+        "energyMin",
+        "energyMax",
+
+        "spectralEntropy",
+        "spectralEntropyNormalized",
+        "spectralEntropyMean",
+        "spectralEntropyMedian",
+        "spectralEntropyStandardDeviation",
+        "spectralEntropyZScore",
+        "spectralEntropyMin",
+        "spectralEntropyMax",
+
+        "spectralCrest",
+        "spectralCrestNormalized",
+        "spectralCrestMean",
+        "spectralCrestMedian",
+        "spectralCrestStandardDeviation",
+        "spectralCrestZScore",
+        "spectralCrestMin",
+        "spectralCrestMax",
+
+        "spectralSkew",
+        "spectralSkewNormalized",
+        "spectralSkewMean",
+        "spectralSkewMedian",
+        "spectralSkewStandardDeviation",
+        "spectralSkewZScore",
+        "spectralSkewMin",
+        "spectralSkewMax",
+
+        "pitchClass",
+        "pitchClassNormalized",
+        "pitchClassMean",
+        "pitchClassMedian",
+        "pitchClassStandardDeviation",
+        "pitchClassZScore",
+        "pitchClassMin",
+        "pitchClassMax",
+
         "bass",
+        "bassNormalized",
+        "bassMean",
+        "bassMedian",
+        "bassStandardDeviation",
+        "bassZScore",
+        "bassMin",
+        "bassMax",
+
         "mids",
+        "midsNormalized",
+        "midsMean",
+        "midsMedian",
+        "midsStandardDeviation",
+        "midsZScore",
+        "midsMin",
+        "midsMax",
+
         "treble",
+        "trebleNormalized",
+        "trebleMean",
+        "trebleMedian",
+        "trebleStandardDeviation",
+        "trebleZScore",
+        "trebleMin",
+        "trebleMax",
+
         // Helper functions
         "getLastFrameColor",
         "rgb2hsl",
         "hsl2rgb",
+        "hslmix",
+        "map",
+
         // Constants
         "PI",
         "mapValue",
         "resolution",
         "time",
+        "random",
       ];
 
       return keywords.map((keyword) => new vscode.CompletionItem(keyword, vscode.CompletionItemKind.Keyword));
     },
   });
 
-  // Register hover provider only for Paper Cranes specific features
+  // Register hover provider
   const hoverProvider = vscode.languages.registerHoverProvider("paper-cranes-fragment-shader", {
     provideHover(document: vscode.TextDocument, position: vscode.Position) {
       console.log("provideHover");
-      const wordRange = document.getWordRangeAtPosition(position);
-      if (!wordRange) return;
+      const range = document.getWordRangeAtPosition(position);
+      if (!range) {
+        return undefined;
+      }
 
-      const word = document.getText(wordRange);
+      const word = document.getText(range);
+      if (!word) {
+        return undefined;
+      }
 
-      // Only provide hover info for Paper Cranes specific features
-      const paperCranesHoverInfo = {
-        rgb2hsl: "vec3 rgb2hsl(vec3 rgb)\nConverts RGB color to HSL color space",
-        hsl2rgb: "vec3 hsl2rgb(vec3 hsl)\nConverts HSL color to RGB color space",
-        getLastFrameColor: "vec4 getLastFrameColor(vec2 uv)\nReturns the color from the previous frame at the specified UV coordinates",
-        mapValue: "float mapValue(float value, float inMin, float inMax, float outMin, float outMax)\nMaps a value from one range to another",
+      const hoverInfo: { [key: string]: string } = {
+        // Audio Analysis - Spectral Features
+        spectralCentroid: "The center of mass of the spectrum. Higher values indicate 'brighter' sounds.",
+        spectralCentroidNormalized: "Normalized spectral centroid value between 0-1.",
+        spectralCentroidZScore: "How many standard deviations from the mean (-1 to 1).",
+
+        spectralFlux: "The rate of change of the spectrum. Higher values indicate more dramatic changes.",
+        spectralFluxNormalized: "Normalized spectral flux value between 0-1.",
+        spectralFluxZScore: "How many standard deviations from the mean (-1 to 1).",
+
+        // Frequency Bands
+        bass: "Low frequency energy (20-250Hz).",
+        bassNormalized: "Normalized bass energy between 0-1.",
+        bassZScore: "How many standard deviations from the mean bass level (-1 to 1).",
+
+        mids: "Mid frequency energy (250-2000Hz).",
+        midsNormalized: "Normalized mids energy between 0-1.",
+        midsZScore: "How many standard deviations from the mean mids level (-1 to 1).",
+
+        treble: "High frequency energy (2000-20000Hz).",
+        trebleNormalized: "Normalized treble energy between 0-1.",
+        trebleZScore: "How many standard deviations from the mean treble level (-1 to 1).",
+
+        // Overall Energy
+        energy: "Overall audio energy across all frequencies.",
+        energyNormalized: "Normalized energy value between 0-1.",
+        energyZScore: "How many standard deviations from the mean energy (-1 to 1).",
+
+        // Helper Functions
+        getLastFrameColor: "Returns the color from the previous frame at the given UV coordinate.\nUsage: getLastFrameColor(vec2 uv)",
+        rgb2hsl: "Converts RGB color to HSL color space.\nUsage: rgb2hsl(vec3 rgb)",
+        hsl2rgb: "Converts HSL color to RGB color space.\nUsage: hsl2rgb(vec3 hsl)",
+        hslmix: "Mixes two colors in HSL space.\nUsage: hslmix(vec3 col1, vec3 col2, float t)",
+        map: "Maps a value from one range to another.\nUsage: map(float value, float inMin, float inMax, float outMin, float outMax)",
+
+        // Constants
+        PI: "Mathematical constant π (3.14159...)",
+        resolution: "Screen resolution in pixels (vec2)",
+        time: "Current time in seconds (float)",
+        random: "Returns a random value between 0-1.\nUsage: random(vec2 st)",
       };
 
-      const info = paperCranesHoverInfo[word];
+      const info = hoverInfo[word];
       if (info) {
         return new vscode.Hover(info);
       }
+
+      return undefined;
     },
   });
 
-  context.subscriptions.push(provider, hoverProvider);
+  // Add providers to disposables array
+  disposables.push(completionProvider, hoverProvider);
+
+  // Add all disposables to subscriptions
+  context.subscriptions.push(...disposables);
 }
 
-export function deactivate() {}
+export function deactivate() {
+  // Clean up will be handled automatically by VS Code
+}
