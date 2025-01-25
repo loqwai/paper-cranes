41ba1874ef5ec5dc946135440c8f6a3c973c5d6d
redaphid <iam@aaronherres.com>
2025-01-24 22:56:48 -0700

    actually log where the server is

--- esbuild.dev.js
+++ esbuild.dev.js
@@ -13,7 +13,7 @@ async function main() {
 
     await ctxCopy.watch()
     await ctxBundle.watch()
-
+    console.log("🦅 Fly up at http://localhost:6969?shader=beat-trip 🦅")
     await ctxBundle.serve({
         servedir: 'dist',
         port: 6969,
