062833cdbd1a41fe8f5a74e082d8a8f793cc1853
redaphid <iam@aaronherres/com>
2025-02-23 02:51:48 -0700

    bump version

--- esbuild.common.js
+++ esbuild.common.js
@@ -53,7 +53,7 @@ export function createBuildOptions(isDev = false) {
         minify: !isDev,
         sourcemap: true ,
         define: {
-            CACHE_NAME: '"2025-02-23:02:44"',
+            CACHE_NAME: '"2025-02-23:02:54"',
             'process.env.NODE_ENV': isDev ? '"development"' : '"production"',
             'process.env.LIVE_RELOAD': isDev ? 'true' : 'false',
         },
