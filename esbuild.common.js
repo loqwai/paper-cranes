f1624135971d3d2fb3f7e45639e227d78977c55e
redaphid <iam@aaronherres.com>
2025-02-25 23:25:54 -0700

    pls just work

--- esbuild.common.js
+++ esbuild.common.js
@@ -52,7 +52,7 @@ export function createBuildOptions(isDev = false) {
         minify: !isDev,
         sourcemap: true,
         define: {
-            CACHE_NAME: '"2025-02-25:23:22"',
+            CACHE_NAME: '"2025-02-25:23:25"',
             'process.env.NODE_ENV': isDev ? '"development"' : '"production"',
             'process.env.LIVE_RELOAD': isDev ? 'true' : 'false',
         },
