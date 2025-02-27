1123ed7b57578439c83598cb5b61e62daa3a5698
redaphid <iam@aaronherres/com>
2025-02-26 22:29:07 -0700

    damn that was rough

--- src/offline-first-fetch.js
+++ src/offline-first-fetch.js
@@ -1,5 +1,8 @@
 
 /** @param {Request} request */
 export const offlineFirstFetch = async (request) => {
-    return request
+    const cache = await caches.open(VERSION)
+    const cachedResponse = await cache.match(request)
+    return cachedResponse
+
 }
--- src/offline-first-fetch.test.js
+++ src/offline-first-fetch.test.js
@@ -1,28 +1,22 @@
 import { describe, it, expect, beforeEach, vi } from "vitest"
 import { offlineFirstFetch } from "./offline-first-fetch"
+
 describe("offline-first-fetch", () => {
+    let cache
+    beforeEach(() => {
+        cache = {
+            match: vi.fn().mockResolvedValue("the-cached-response")
+        }
+        globalThis.caches /** @type {Partial<CacheStorage>} */ = {
+            open: vi.fn().mockResolvedValue(cache)
+        }
+        globalThis.VERSION = "twiddle-grr"
+    })
     it("should exist", () => {
         expect(offlineFirstFetch).toBeDefined()
     })
 
-    describe("when the url is not cached", () => {
-        it("should give us a response", async () => {
-            const request = new Request("https://famous-beads.com")
-            const response = await offlineFirstFetch(request)
-            expect(response).toBeDefined()
-        })
-    })
     describe("when the url is cached", () => {
-        beforeEach(() => {
-            /** @type {Partial<CacheStorage>} */
-            const caches = {
-                open: vi.fn(),
-            };
-
-            // I hate you, TypeScript.
-            globalThis.caches = /** @type {CacheStorage} */ (caches)
-        });
-
         it("should give us a response", async () => {
             const request = new Request("https://famous-beads.com")
             const response = await offlineFirstFetch(request)
