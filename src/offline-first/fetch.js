1b5095d934437d4325234c57186fd4a243df63e1
redaphid <iam@aaronherres/com>
2025-02-26 23:33:03 -0700

    supposedly works

--- src/offline-first/fetch.js
+++ src/offline-first/fetch.js
@@ -4,13 +4,11 @@ import reloadPage from "./reload-page"
 export const offlineFirstFetch = async (request) => {
     const cache = await caches.open(VERSION)
     const cachedResponse = await cache.match(request)
-    fetch(request).then(async (response) => {
+    const netPromise = fetch(request).then(async (response) => {
         const shouldReload = await addToCache({request,response,cache})
         if(shouldReload) reloadPage()
+        return response
     })
     if(cachedResponse) return cachedResponse
-
-    const networkResponse = await fetch(request)
-    return networkResponse
-
+    return netPromise
 }
