6924031e8afdb3eb43ef6359f680b8f6039ddcfa
redaphid <iam@aaronherres.com>
2025-03-20 18:08:23 -0700

    move message handling to worker comms

--- src/worker-communication.js
+++ src/worker-communication.js
@@ -26,3 +26,20 @@ const processServiceWorkerMessage = (event) => {
   }
   console.log('Received strange message from service worker', event.data)
 }
+
+// Add listener for messages from parent window
+window.addEventListener('message', function(event) {
+  if(!window.cranes) return
+  if (!event.data || event.data.type !== 'update-params') return
+
+  // Store incoming params
+  const {data } = event.data
+
+  // Update shader code if provided
+  if (data.shaderCode) window.cranes.shader = data.shaderCode
+
+  // Store all params
+  Object.entries(data).forEach(([key, value]) => {
+    window.cranes.messageParams[key] = value
+  })
+})
